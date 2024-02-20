e9c7d68ed06887793301127655f4c0523d701743
hanke0 <hanke0@outlook.com>
2024-02-20 21:05:12 +0800

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import type { Metadata } from "next";
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 
 export const metadata: Metadata = {
   title: "WebDrop",
   description: "A peer-to-peer file sharing ad hoc service.",
 };
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(<Component {...pageProps} />)
 }
