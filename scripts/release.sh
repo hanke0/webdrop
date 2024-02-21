6cddf049636847d08312821a466fc3e2b203e98b
hanke0 <hanke0@outlook.com>
2024-02-21 14:48:51 +0800

    fix -r

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,8 +7,8 @@ yarn build
 
 rm -rf dist
 mkdir dist
-cp -cr build dist/
-cp -cr node_modules dist/node_modules
+cp -r build dist/
+cp -r node_modules dist/node_modules
 cp package.json dist/package.json
 cp yarn.lock dist/yarn.lock
 cp server.js dist/server.js
